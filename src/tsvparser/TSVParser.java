2e6c6814c7dd0477a50318adf2acc82adc9f1d16
Malcolm Riley <malcolmriley@users.noreply.github.com>
2017-06-04 20:00:17 -0700

    Commit - building TableEntry objects from file is working

--- src/tsvparser/TSVParser.java
+++ src/tsvparser/TSVParser.java
@@ -9,19 +9,25 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 
+import tsvparser.utilities.Entries;
+import tsvparser.utilities.TSVUtilities;
 import tsvparser.utilities.TableEntry;
 
 public class TSVParser {
 
 	public static void main(String[] passedArguments) {
 		String testpath = "/Users/macintosh/Documents/Java Projects/gamecip/corpus/corpus_0.tsv";
-		buildEntries(Paths.get(testpath));
+		for (TableEntry iteratedEntry : buildEntries(Paths.get(testpath))) {
+			System.out.println(iteratedEntry.getEntry(Entries.PAGE_ID));
+		}
 	}
 	
 	public static List<TableEntry> buildEntries(Path passedFilePath) {
 		ArrayList<TableEntry> entries = new ArrayList<TableEntry>();
 		try (BufferedReader reader = Files.newBufferedReader(passedFilePath)) {
 			Stream<String> lines = reader.lines();
+			// Skip header row - contains labels that are not needed
+			lines.skip(1).forEach(string -> entries.add(TSVUtilities.fromLine(string)));
 		}
 		catch (IOException passedException) {
 			passedException.printStackTrace();
