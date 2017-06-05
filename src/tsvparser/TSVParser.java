e395fd29c2f26fb6cb6f6aad80a3fe5dafeea525
Malcolm Riley <malcolmriley@users.noreply.github.com>
2017-06-05 15:13:53 -0700

    Commit - test for working imports

--- src/tsvparser/TSVParser.java
+++ src/tsvparser/TSVParser.java
@@ -1,9 +1,11 @@
 package tsvparser;
 
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 
 import tsvparser.utilities.TSVUtilities;
+import tsvparser.utilities.TableEntry;
 
 public class TSVParser {
 	
@@ -12,6 +14,7 @@ public class TSVParser {
 	public static void main(String[] passedArguments) {
 		String testpath = "/Users/macintosh/Documents/Java Projects/gamecip/corpus/";
 		List<Path> discoveredFiles = TSVUtilities.getFilesInDirectory(testpath, TSV_EXTENSION);
-		discoveredFiles.stream().forEach(System.out::println);
+		List<TableEntry> generatedEntries = new ArrayList<TableEntry>();
+		discoveredFiles.stream().forEach(path -> TSVUtilities.buildEntries(generatedEntries, path));
 	}
 }
