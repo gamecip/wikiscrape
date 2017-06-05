36bd71e35a08faced2fd3607207eacbf051278be
Malcolm Riley <malcolmriley@users.noreply.github.com>
2017-06-04 17:21:15 -0700

    QueryFactory cleanup

--- src/queries/QueryFactory.java
+++ src/queries/QueryFactory.java
@@ -13,9 +13,26 @@ public class QueryFactory {
 	private QueryFactory() {
 	};
 	
+	// Strings
 	private static final String ACTION = "action";
 	private static final String QUERY = "query";
+	private static final String EXTRACT = "extract";
+	private static final String PROPERTIES = "prop";
 	
+	// Arguments
+	public static final Argument REVISION_IDS = new Argument("ids", "revids");
+	public static final Argument REVISION_FLAGS = new Argument("flags", "minor");
+	public static final Argument REVISION_SIZE = new Argument("size", "size");
+	
+	public static final Argument EXTRACTS_PLAIN = new Argument("plain", EXTRACT);
+	public static final Argument EXTRACTS_WIKI = new Argument("wiki", EXTRACT);
+	public static final Argument EXTRACTS_RAW = new Argument("raw", EXTRACT);
+	
+	// Known Queries
+	public static final Query GET_PLAINTEXT = new Query("explaintext", EXTRACT);
+	public static final Query GET_REVISIONS = new Query("rvprop", "revisions");
+	
+	// Specifying Pages
 	public static enum PageSpecification {
 
 		BY_IDS("pageids"),
@@ -38,19 +55,4 @@ public class QueryFactory {
 		String pageSpecification = Utilities.getQueryTerm(passedPageSpecification.getType(), passedPageStrings);
 		return Utilities.concatenateCommands(baseURL, pageSpecification);
 	}
-
-//	/* Revision metadata queries */
-//	public static final QueryCommand OPTION_REVISIONS_IDS = new QueryCommand("ids", "revids");
-//	public static final QueryCommand OPTION_REVISIONS_FLAGS = new QueryCommand("flags", "minor");
-//	public static final QueryCommand OPTION_REVISIONS_SIZE = new QueryCommand("size", "size");
-//	public static final QueryCommand ARGUMENT_REVISIONS = new QueryCommand("rvprop", "revisions", OPTION_REVISIONS_IDS, OPTION_REVISIONS_FLAGS, OPTION_REVISIONS_SIZE);
-//
-//	/* Page content extraction queries */
-//	public static final QueryCommand OPTION_EXTRACTS_PLAIN = new QueryCommand("plain", "extract");
-//	public static final QueryCommand OPTION_EXTRACTS_WIKI = new QueryCommand("wiki", "extract");
-//	public static final QueryCommand OPTION_EXTRACTS_RAW = new QueryCommand("raw", "extract");
-//	public static final QueryCommand ARGUMENT_SECTIONFORMAT = new QueryCommand("exsectionformat", "extract", OPTION_EXTRACTS_PLAIN, OPTION_EXTRACTS_WIKI, OPTION_EXTRACTS_RAW);
-//	public static final QueryCommand ARGUMENT_PLAINTEXT = new QueryCommand("explaintext", "extract");
-//
-//	public static final String TYPE_PROPERTIES = "prop";
 }
