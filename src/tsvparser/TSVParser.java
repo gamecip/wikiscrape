fb4babb06b96e23fd0b0d7b2329b72ba1f448313
Malcolm Riley <malcolmriley@users.noreply.github.com>
2017-06-05 10:21:43 -0700

    File extension uses private static field

--- src/tsvparser/TSVParser.java
+++ src/tsvparser/TSVParser.java
@@ -6,10 +6,12 @@ import java.util.List;
 import tsvparser.utilities.TSVUtilities;
 
 public class TSVParser {
+	
+	private static String TSV_EXTENSION = "tsv";
 
 	public static void main(String[] passedArguments) {
 		String testpath = "/Users/macintosh/Documents/Java Projects/gamecip/corpus/";
-		List<Path> discoveredFiles = TSVUtilities.getFilesInDirectory(testpath, "tsv");
+		List<Path> discoveredFiles = TSVUtilities.getFilesInDirectory(testpath, TSV_EXTENSION);
 		discoveredFiles.stream().forEach(System.out::println);
 	}
 }
