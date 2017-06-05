650b3f5048ab1013d3f12be2da714d954bad8146
Malcolm Riley <malcolmriley@users.noreply.github.com>
2017-06-04 19:57:27 -0700

    Superior implementation

--- src/tsvparser/utilities/TableEntry.java
+++ src/tsvparser/utilities/TableEntry.java
@@ -9,11 +9,34 @@ public class TableEntry {
 	
 	private final String[] ENTRIES;
 	
+	/**
+	 * Creates a new TableEntry object initialized with the passed {@code String} array as its entries.
+	 * 
+	 * @param passedEntries - The entries to use
+	 */
 	public TableEntry(String[] passedEntries) {
 		this.ENTRIES = passedEntries;
 	}
 	
+	/**
+	 * Returns an entry from this TableEntry corresponding to the passed {@code int}.
+	 * <p>
+	 * The passed {@code int} can be thought of as the column number, starting at zero.
+	 * <p>
+	 * This method will return a null {@code String} if the passed index is negative or otherwise
+	 * out of range.
+	 * @param passedEntryIndex - The index of the entry to retrieve
+	 * @return The String stored at that column index.
+	 */
 	public String getEntry(int passedEntryIndex) {
-		return this.ENTRIES[passedEntryIndex];
+		if (validate(passedEntryIndex)) {
+			return this.ENTRIES[passedEntryIndex];
+		}
+		return null;
+	}
+	
+	// Logic Methods
+	private boolean validate(int passedInteger) {
+		return (passedInteger >= 0) && (passedInteger < this.ENTRIES.length);
 	}
 }
