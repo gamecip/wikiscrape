2c5b94e44092eaf2561fb9d9e1020ec7e80d8eda
Malcolm Riley <malcolmriley@users.noreply.github.com>
2017-06-05 16:59:44 -0700

    Rename to reflect actual functionality and avoid namespace collisions

--- src/wikiscrape/queries/QueryBuilder.java
+++ src/wikiscrape/queries/QueryBuilder.java
@@ -5,19 +5,19 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.function.Function;
+import java.util.stream.Stream;
 
 import wikiscrape.utilities.ScrapeUtilities;
 
 /**
- * Flexible class for defining a wiki api query.
- * 
- * Recursive - supports adding other Query objects as options.
- * 
+ * Flexible class for defining a wiki api query. It is also recursive; it supports adding other {@link QueryBuilder} objects as options.
+ * <p>
  * Various levels of caching are used to avoid rebuilding Strings when mutable aspects are changed, i.e. the arguments of the command or the options passed to it.
- * 
+ * <p>
+ * Calling {@link #build()} will return a {@code String} representation of the desired query.
  * @author Malcolm Riley
  */
-public class Query {
+public class QueryBuilder {
 	
 	// Functions
 	private static final Function<Argument, String> ARGUMENT_MAPPER = (Argument argument) -> {return argument.getArgumentSyntax();};
@@ -25,7 +25,7 @@ public class Query {
 	// Local values
 	private Argument COMMAND;
 	private Argument[] ARGUMENTS;
-	private List<Query> OPTIONS;
+	private List<QueryBuilder> OPTIONS;
 
 	// Cached Results
 	private String CACHE_FINAL = null;
@@ -42,101 +42,105 @@ public class Query {
 	private static final String EXCEPTION_ILLEGAL_ARGUMENT = "Argument may not be null, neither may it have empty or null syntax or results.";
 	
 	/**
-	 * Constructs a new {@link Query} without child arguments or options, with the passed {@link Argument} representing its command and result syntaxes.
+	 * Constructs a new {@link QueryBuilder} without child arguments or options, with the passed {@link Argument} representing its command and result syntaxes.
 	 * 
-	 * @param passedCommand - An {@link Argument} representing the syntax to be used for this {@link Query}'s command and results
+	 * @param passedCommand - An {@link Argument} representing the syntax to be used for this {@link QueryBuilder}'s command and results
 	 */
-	public Query(Argument passedArgument) {
-		this(passedArgument, null, (Query[])null);
+	public QueryBuilder(Argument passedArgument) {
+		this(passedArgument, null, (QueryBuilder[])null);
 	}
 
 	/**
-	 * Constructs a new {@link Query} without arguments or options, with the passed {@link Argument} representing its command and result syntaxes.
+	 * Constructs a new {@link QueryBuilder} without arguments or options, with the passed {@link Argument} representing its command and result syntaxes.
 	 * 
 	 * @param passedCommandSyntax - The syntax of the command to be used
 	 * @param passedResultSyntax - The expected result field given a successful query
 	 * 
 	 * @throws IllegalArgumentException - If passedCommandSyntax or passedResultSyntax are empty or null.
 	 */
-	public Query(String passedCommandSyntax, String passedResultSyntax) {
-		this(new Argument(passedCommandSyntax, passedResultSyntax), null, (Query[])null);
+	public QueryBuilder(String passedCommandSyntax, String passedResultSyntax) {
+		this(new Argument(passedCommandSyntax, passedResultSyntax), null, (QueryBuilder[])null);
 	}
 	
 	/**
-	 * Constructs a new {@link Query} with the passed arguments and no options.
+	 * Constructs a new {@link QueryBuilder} with the passed arguments and no options.
 	 * 
-	 * @param passedCommand - An {@link Argument} representing the syntax to be used for this {@link Query}'s command and results
+	 * @param passedCommand - An {@link Argument} representing the syntax to be used for this {@link QueryBuilder}'s command and results
 	 * @param passedArguments - The arguments to be used for this command
 	 * 
 	 * @throws IllegalArgumentException - If passedCommandSyntax or passedResultSyntax are empty or null.
 	 */
-	public Query(Argument passedArgument, Argument[] passedArguments) {
-		this(passedArgument, passedArguments, (Query[])null);
+	public QueryBuilder(Argument passedArgument, Argument[] passedArguments) {
+		this(passedArgument, passedArguments, (QueryBuilder[])null);
 	}
 
 	/**
-	 * Constructs a new {@link Query} with the passed arguments and options.
+	 * Constructs a new {@link QueryBuilder} with the passed arguments and options.
 	 * 
-	 * @param passedCommand - An {@link Argument} representing the syntax to be used for this {@link Query}'s command and results
+	 * @param passedCommand - An {@link Argument} representing the syntax to be used for this {@link QueryBuilder}'s command and results
 	 * @param passedArguments - The arguments to be used for this command
-	 * @param passedOptions - The attached {@link Query}s to be used as options.
+	 * @param passedOptions - The attached {@link QueryBuilder}s to be used as options.
 	 * 
 	 * @throws IllegalArgumentException - If passedCommandSyntax or passedResultSyntax are empty or null.
 	 */
-	public Query(Argument passedCommand, Argument[] passedArguments, Query... passedOptions) {
-		if (validateArgument(passedCommand)) {
+	public QueryBuilder(Argument passedCommand, Argument[] passedArguments, QueryBuilder... passedOptions) {
+		if (!validateArgument(passedCommand)) {
 			throw new IllegalArgumentException(EXCEPTION_ILLEGAL_ARGUMENT);
 		}
 		this.COMMAND = passedCommand;
-		this.setArguments(passedArguments);
-		this.setOptions(passedOptions);
+		if (passedArguments != null) {
+			this.setArguments(passedArguments);
+		}
+		if (passedOptions != null) {
+			this.setOptions(passedOptions);
+		}
 		this.CHANGED_RESULTS = true;
 		this.CHANGED_ARGUMENTS = true;
 		this.CHANGED_OPTIONS = true;
 	}
 	
 	/**
-	 * Removes all arguments from this {@link Query}.
+	 * Removes all arguments from this {@link QueryBuilder}.
 	 */
 	public void resetArguments() {
 		this.setArguments((Argument[])null);
 	}
 	
 	/**
-	 * Removes all options from this {@link Query}.
+	 * Removes all options from this {@link QueryBuilder}.
 	 */
 	public void resetOptions() {
-		this.setOptions((Query[]) null);
+		this.setOptions((QueryBuilder[]) null);
 	}
 
 	/**
-	 * Sets the arguments of this {@link Query} to the passed arguments.
+	 * Sets the arguments of this {@link QueryBuilder} to the passed arguments.
 	 * 
-	 * @param passedArguments - The new arguments to use for this {@link Query}.
+	 * @param passedArguments - The new arguments to use for this {@link QueryBuilder}.
 	 * @return {@code this}, for the purpose of method chaining.
 	 */
-	public Query setArguments(Argument ... passedArguments) {
+	public QueryBuilder setArguments(Argument ... passedArguments) {
 		this.ARGUMENTS = passedArguments;
 		this.CHANGED_ARGUMENTS = true;
 		return this;
 	}
 
 	/**
-	 * Sets the options of this {@link Query} to the passed {@link Query}s.
+	 * Sets the options of this {@link QueryBuilder} to the passed {@link QueryBuilder}s.
 	 * 
-	 * @param passedOptions - the {@link Query}s to set as the options for this {@link Query} intance.
+	 * @param passedOptions - the {@link QueryBuilder}s to set as the options for this {@link QueryBuilder} intance.
 	 * @return {@code this}, for the purpose of method chaining.
 	 */
-	public Query setOptions(Query... passedOptions) {
+	public QueryBuilder setOptions(QueryBuilder... passedOptions) {
 		this.OPTIONS = Collections.unmodifiableList(Arrays.asList(passedOptions));
 		this.CHANGED_OPTIONS = true;
 		return this;
 	}
 
 	/**
-	 * Returns the total {@code String} form build of this {@link Query}, including all current arguments and options.
+	 * Returns the total {@code String} form build of this {@link QueryBuilder}, including all current arguments and options.
 	 * 
-	 * @return This {@link Query}, in finalized {@code String} form.
+	 * @return This {@link QueryBuilder}, in finalized {@code String} form.
 	 */
 	public String build() {
 		boolean requiresRebuild = (this.CHANGED_RESULTS || this.CHANGED_ARGUMENTS || this.CHANGED_OPTIONS);
@@ -150,15 +154,15 @@ public class Query {
 	}
 
 	/**
-	 * Returns an immutable, unmodifiable {@link List} containing all currently attached {@link Query} options.
+	 * Returns an immutable, unmodifiable {@link List} containing all currently attached {@link QueryBuilder} options.
 	 * @return
 	 */
-	public List<Query> getOptions() {
+	public List<QueryBuilder> getOptions() {
 		return this.OPTIONS;
 	}
 
 	/**
-	 * Returns the simple command syntax of this {@link Query} alone, without any arguments or options.
+	 * Returns the simple command syntax of this {@link QueryBuilder} alone, without any arguments or options.
 	 * 
 	 * @return The syntax of this command.
 	 */
@@ -167,36 +171,36 @@ public class Query {
 	}
 
 	/**
-	 * Returns the expected result key for this {@link Query} alone, not including any attached options. This is the field in the returned json that corresponds
-	 * to the result of this particular {@link Query}.
+	 * Returns the expected result key for this {@link QueryBuilder} alone, not including any attached options. This is the field in the returned json that corresponds
+	 * to the result of this particular {@link QueryBuilder}.
 	 * 
-	 * @return The result key for this {@link Query}.
+	 * @return The result key for this {@link QueryBuilder}.
 	 */
 	public String getResult() {
 		return this.COMMAND.getResultSyntax();
 	}
 
 	/**
-	 * Returns whether this {@link Query} has any other {@link Query}s attached as options.
+	 * Returns whether this {@link QueryBuilder} has any other {@link QueryBuilder}s attached as options.
 	 * 
-	 * @return Whether this {@link Query} has options attached.
+	 * @return Whether this {@link QueryBuilder} has options attached.
 	 */
 	public boolean hasOptions() {
 		return (this.OPTIONS != null) && (!this.OPTIONS.isEmpty());
 	}
 
 	/**
-	 * Returns whether this {@link Query} currently has any arguments.
+	 * Returns whether this {@link QueryBuilder} currently has any arguments.
 	 * 
-	 * @return Whether this {@link Query} has any arguments.
+	 * @return Whether this {@link QueryBuilder} has any arguments.
 	 */
 	public boolean hasArguments() {
 		return (this.ARGUMENTS != null) && (this.ARGUMENTS.length > 0);
 	}
 	
 	@Override
-	public Query clone() {
-		Query newQuery = new Query(this.COMMAND, this.ARGUMENTS, this.OPTIONS.toArray(new Query[]{}));
+	public QueryBuilder clone() {
+		QueryBuilder newQuery = new QueryBuilder(this.COMMAND);
 		newQuery.setCacheArguments(this.buildArguments());
 		newQuery.setCacheCommand(this.buildCommand());
 		newQuery.setCacheOptions(this.buildOptions());
@@ -259,7 +263,7 @@ public class Query {
 		if (this.CHANGED_OPTIONS) {
 			if (this.hasOptions()) {
 				ArrayList<String> discoveredOptions = new ArrayList<String>();
-				for (Query iteratedQuery : this.getOptions()) {
+				for (QueryBuilder iteratedQuery : this.getOptions()) {
 					discoveredOptions.add(iteratedQuery.build());
 				}
 				if (!discoveredOptions.isEmpty()) {
@@ -277,7 +281,7 @@ public class Query {
 		if (passedArgument == null) {
 			return false;
 		}
-		return validateString(passedArgument.getArgumentSyntax()) && validateString(passedArgument.getResultSyntax());
+		return validateString(passedArgument.getArgumentSyntax());
 	}
 	
 	private static boolean validateString(String passedString) {
@@ -297,7 +301,8 @@ public class Query {
 	}
 	
 	private static String[] buildArgumentArray(Argument ... passedArguments) {
-		return (String[]) Arrays.stream(passedArguments).map(ARGUMENT_MAPPER).toArray();
+		Stream<String> generatedStream = Arrays.stream(passedArguments).map(ARGUMENT_MAPPER);
+		return generatedStream.toArray(val -> { return new String[val]; });
 	}
 
 }
