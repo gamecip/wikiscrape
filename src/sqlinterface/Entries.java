3d88451571813254445896c1db2c8bd2db4b37eb
Malcolm Riley <malcolmriley@users.noreply.github.com>
2017-06-06 13:24:12 -0700

    Add names to entry types

--- src/sqlinterface/Entries.java
+++ src/sqlinterface/Entries.java
@@ -2,12 +2,20 @@ package sqlinterface;
 
 public class Entries {
 	
-	public static final int NAME = 0;
+	public static final int TITLE = 0;
 	public static final int PAGE_ID = 1;
 	public static final int REVISION_ID = 2;
 	public static final int YEAR = 3;
 	public static final int TEXT_INTRO = 4;
 	public static final int TEXT_FULL = 5;
 	public static final int CATEGORIES = 6;
+	
+	public static final String NAME_TITLE = "title";
+	public static final String NAME_PAGE_ID = "pageid";
+	public static final String NAME_REVISION_ID = "revisionid";
+	public static final String NAME_YEAR = "year";
+	public static final String NAME_TEXT_INTRO = "textintro";
+	public static final String NAME_TEXT_FULL = "textfull";
+	public static final String NAME_CATEGORIES = "categories";
 	
 }
