d83f4a1ad559031200554b356e439767fc4660c8
Malcolm Riley <malcolmriley@users.noreply.github.com>
2017-06-05 18:16:26 -0700

    Miscellaneous cleanups

--- src/wikiscrape/queries/Queries.java
+++ src/wikiscrape/queries/Queries.java
@@ -18,7 +18,9 @@ public class Queries {
 	// Known Actions
 	public static final String QUERY = "query";
 	
-	// Known Arguments
+	// Known Arguments	
+	public static final Argument EXTRACTS = new Argument("extracts");
+	
 	public static final Argument PAGES_BY_ID = new Argument("pageids");
 	public static final Argument PAGES_BY_TITLE = new Argument("titles");
 	public static final Argument PAGES_BY_REVISION_ID = new Argument("revids");
@@ -27,16 +29,18 @@ public class Queries {
 	public static final Argument REVISION_FLAGS = new Argument("flags", "minor");
 	public static final Argument REVISION_SIZE = new Argument("size", "size");
 	
-	public static final Argument OPTION_EXTRACTS_PLAIN = new Argument("plain", STRING_EXTRACT);
-	public static final Argument OPTION_EXTRACTS_WIKI = new Argument("wiki", STRING_EXTRACT);
-	public static final Argument OPTION_EXTRACTS_RAW = new Argument("raw", STRING_EXTRACT);
+	public static final Argument OPTION_SECTIONFORMAT_PLAIN = new Argument("plain", STRING_EXTRACT);
+	public static final Argument OPTION_SECTIONFORMAT_WIKI = new Argument("wiki", STRING_EXTRACT);
+	public static final Argument OPTION_SECTIONFORMAT_RAW = new Argument("raw", STRING_EXTRACT);
 	
 	public static final Argument REVISIONS = new Argument(STRING_REVISIONS, STRING_REVISIONS);
 	
-	// Known "Get" Queries
-	public static final QueryBuilder GET_PLAINTEXT = new QueryBuilder("explaintext", STRING_EXTRACT);
+	// Known "Get" Queries - Clone these
 	public static final QueryBuilder GET_PROPERTIES = new QueryBuilder(new Argument(STRING_PROPERTIES));
 	
-	// Optional Queries
+	// Optional Queries - Clone these
 	public static final QueryBuilder OPTION_REVISIONS = new QueryBuilder("rvprop", STRING_REVISIONS);
+	public static final QueryBuilder OPTION_SECTIONFORMAT = new QueryBuilder("exsectionformat", STRING_EXTRACT);
+	public static final QueryBuilder OPTION_EXTRACTLIMIT = new QueryBuilder("exlimit", STRING_EXTRACT);
+	public static final QueryBuilder OPTION_EXTRACT_PLAINTEXT = new QueryBuilder("explaintext", STRING_EXTRACT);
 }
