f760264e7663985c73235b485c08371404e6f938
Malcolm Riley <malcolmriley@users.noreply.github.com>
2017-06-06 01:33:10 -0700

    Comments and skeleton

--- src/tsvparser/TSVParser.java
+++ src/tsvparser/TSVParser.java
@@ -12,9 +12,12 @@ public class TSVParser {
 	private static String TSV_EXTENSION = "tsv";
 
 	public static void main(String[] passedArguments) {
+		// Generate table entries
 		String testpath = "/Users/macintosh/Documents/Java Projects/gamecip/corpus/";
 		List<Path> discoveredFiles = TSVUtilities.getFilesInDirectory(testpath, TSV_EXTENSION);
 		List<TableEntry> generatedEntries = new ArrayList<TableEntry>();
 		discoveredFiles.stream().forEach(path -> TSVUtilities.buildEntries(generatedEntries, path));
+		
+		// Push entries to database
 	}
 }
--- src/wikiscrape/WikiScraper.java
+++ src/wikiscrape/WikiScraper.java
@@ -1,11 +1,19 @@
 package wikiscrape;
 
+import wikiscrape.queries.Queries;
+import wikiscrape.queries.QueryBuilder;
+import wikiscrape.utilities.RequestManager;
+
 public class WikiScraper {
 
 	public static void main(String[] passedArguments) {
-		// Construct internal objects
 		
-		// Build page list from database
+		// Construct internal objects
+		String wiki_url = "en.wikipedia.org";
+		RequestManager requestManager = new RequestManager(wiki_url);
+		QueryBuilder query = new QueryBuilder(Queries.PAGES_BY_ID);
+		
+		// Build page update list from database
 		
 		// Of page list, compare revision ids to build list of extracts to redownload
 		
