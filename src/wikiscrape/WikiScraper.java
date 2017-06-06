8fe5fcb2ea57b655b16f5cfc203a6bd0b7aceb0b
Malcolm Riley <malcolmriley@users.noreply.github.com>
2017-06-05 18:37:38 -0700

    Skeleton

--- src/wikiscrape/WikiScraper.java
+++ src/wikiscrape/WikiScraper.java
@@ -3,6 +3,14 @@ package wikiscrape;
 public class WikiScraper {
 
 	public static void main(String[] passedArguments) {
-		String[] pageList = { "3", "5", "7" };
+		// Construct internal objects
+		
+		// Build page list from database
+		
+		// Of page list, compare revision ids to build list of extracts to redownload
+		
+		// Redownload extracts
+		
+		// Push to database
 	}
 }
