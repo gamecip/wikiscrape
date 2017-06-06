241b30aede7ebc56357587c68d351ed7f735c672
Malcolm Riley <malcolmriley@users.noreply.github.com>
2017-06-05 18:43:53 -0700

    Fix build() to be sensitive to child options changing

--- src/wikiscrape/queries/QueryBuilder.java
+++ src/wikiscrape/queries/QueryBuilder.java
@@ -142,7 +142,7 @@ public class QueryBuilder {
 	 * @return This {@link QueryBuilder}, in finalized {@code String} form.
 	 */
 	public String build() {
-		boolean requiresRebuild = (this.CHANGED_ARGUMENTS || this.CHANGED_OPTIONS);
+		boolean requiresRebuild = (this.CHANGED_ARGUMENTS || this.CHANGED_OPTIONS || this.haveOptionsChanged());
 		if (requiresRebuild) {
 			this.CACHE_FINAL = this.buildCommand();
 			if (this.hasOptions()) {
