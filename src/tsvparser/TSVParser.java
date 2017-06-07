e0b22ca7765f783b68df5ebf232f63a664c01302
Malcolm Riley <malcolmriley@users.noreply.github.com>
2017-06-06 23:48:32 -0700

    Final version of TSVParser

--- src/tsvparser/TSVParser.java
+++ src/tsvparser/TSVParser.java
@@ -1,9 +1,11 @@
 package tsvparser;
 
 import java.nio.file.Path;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import sqlinterface.SQLInterface;
 import sqlinterface.TableEntry;
 import tsvparser.utilities.TSVUtilities;
 
@@ -12,13 +14,32 @@ public class TSVParser {
 	private static String TSV_EXTENSION = "tsv";
 
 	public static void main(String[] passedArguments) {
-		// Generate table entries
-		String testpath = "/Users/macintosh/Documents/Java Projects/gamecip/corpus/";
-		List<Path> discoveredFiles = TSVUtilities.getFilesInDirectory(testpath, TSV_EXTENSION);
-		List<TableEntry> generatedEntries = new ArrayList<TableEntry>();
-		discoveredFiles.forEach(path -> TSVUtilities.buildEntries(generatedEntries, path));
-		generatedEntries.forEach(entry -> entry.swapEntries(0, 1)); // Swap first two entries so that the primary index is page ID (nonvolatile) and not page title (volatile)
+		// Construct objects
+		String path = "/Users/macintosh/Documents/Java Projects/gamecip/corpus/";
 		
-		// Push entries to database
+		// TODO!
+		String sqlurl = "";
+		String tablename = "";
+		String username = "";
+		String password = "";
+		
+		List<Path> discoveredFiles = TSVUtilities.getFilesInDirectory(path, TSV_EXTENSION);
+		List<TableEntry> generatedEntries = new ArrayList<TableEntry>();
+		
+		try {
+			SQLInterface sqlInterface = new SQLInterface(sqlurl, tablename, username, password);
+			
+			// Generate entries
+			discoveredFiles.forEach(filepath -> TSVUtilities.buildEntries(generatedEntries, filepath));
+			generatedEntries.forEach(entry -> entry.swapEntries(0, 1)); // Swap first two entries so that the primary index is page ID (nonvolatile) and not page title (volatile)
+			
+			// Push entries to database
+			sqlInterface.insert(generatedEntries.toArray(new TableEntry[]{}));
+		}
+		catch (SQLException passedException) {
+			passedException.printStackTrace();
+			// If the SQLInterface cannot be constructed, there's no point in continuing
+			return;
+		}
 	}
 }
