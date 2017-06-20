e145ab8b55fe2806257e9014b96ef92dff7de87b
Malcolm Riley <malcolmriley@users.noreply.github.com>
2017-06-19 17:40:13 -0700

    Fix null OPTION_EXTRACT_INTRO
    How could this happen

--- src/wikiscrape/queries/Queries.java
+++ src/wikiscrape/queries/Queries.java
@@ -57,7 +57,7 @@ public class Queries {
 	public static final QueryBuilder OPTION_SECTIONFORMAT = new QueryBuilder("exsectionformat", FIELD_EXTRACT);
 	public static final QueryBuilder OPTION_EXTRACTLIMIT = new QueryBuilder("exlimit", FIELD_EXTRACT);
 	public static final QueryBuilder OPTION_EXTRACT_PLAINTEXT = new QueryBuilder("explaintext", FIELD_EXTRACT);
-	public static final QueryBuilder OPTION_EXTRACT_INTRO = null;
+	public static final QueryBuilder OPTION_EXTRACT_INTRO = new QueryBuilder("exintro", FIELD_EXTRACT);
 	public static final QueryBuilder OPTION_CONTINUE = new QueryBuilder(FIELD_CONTINUE, FIELD_CONTINUE);
 	public static final QueryBuilder OPTION_CONTINUE_FROM = new QueryBuilder(FIELD_CONTINUE_FROM, FIELD_CONTINUE_FROM);
 	
